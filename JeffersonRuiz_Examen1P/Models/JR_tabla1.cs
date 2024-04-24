e75f41d56146bae56fc8959e67c27434e7928e5c
JeffKun1999 <jefferson.ruiz.pico@udla.edu.ec>
2024-04-24 10:56:52 -0500

    Creado el model con los 5 atributos, INT, String, Bool, Decimal y fecha.

---
+++ JeffersonRuiz_Examen1P/Models/JR_tabla1.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JeffersonRuiz_Examen1P.Models
+{
+    public class JR_tabla1
+    {
+        public int JR_Int_Atributo1 { get; set; }
+        [Required]
+        public string? JR_String_Atributo2 { get; set; }
+
+        public bool JR_Booleano_Atributo3 {  get; set; }
+        [Range(0.01,20)]
+        public decimal JR_Decimal_Atributo4 {  get; set; }
+        public DateTime JR_Fecha_Atributo5 { get; set; }
+
+
+    }
+}
